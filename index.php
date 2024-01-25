e478e8cd53f70890c2a5b61596f8cc9aebaa74fe
CoderShanto <sshanto12344321@gmail.com>
2024-01-26 00:02:26 +0600

    start

--- index.php
+++ index.php
@@ -3,15 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Shopping Cart</title>
+    <title>Shoping Cart</title>
+
+    <!--css file-->
+    <link rel="stylesheet" href="css/style.css">
+
 </head>
 <body>
-
-    <h1>Welcome to My project</h1>
-    
-    <p>This is a simple HTML page created for demonstration purposes.</p>
-    
-    <img src="https://via.placeholder.com/150" alt="Placeholder Image">
+    <h1>Hello World</h1>
+    <!--js file-->
+    <script src="js/script.js"></script>
     
 </body>
 </html>
